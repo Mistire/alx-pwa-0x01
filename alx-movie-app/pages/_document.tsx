d2d16c73e17dca017a5a20ccbdce2521cbbc891d
Mistire <mdiwithgod@gmail.com>
2025-10-26 22:51:51 +0300

    Link the Manifest in _document.js

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
